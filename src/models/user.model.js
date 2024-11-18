dd5707c13ec59b189e48f27e257678c5c8a7662f
Kaium Al Limon <limon.3cs@gmail.com>
2024-11-18 12:21:44 +0600

    match password function add

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,42 +3,53 @@ const bcrypt = require('bcrypt');
 
 // create the user schema
 const userSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: true
     },
-    phone:{
+    phone: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
         required: true
     },
-    gender:{
+    gender: {
         type: String,
         required: true
     },
-    dob:{
+    dob: {
         type: Date,
         required: true
-    },
-})
-
+    }
+});
 
 // hash the password before saving
-
-userSchema.pre('save',async (next)=>{
-    if(!this.isModified('password')){
-        next()
+userSchema.pre('save', async function(next) {
+    // Check if the password is modified (new or changed)
+    if (!this.isModified('password')) {
+        return next();  // if not modified, skip the hashing
     }
-    this.password = await bcrypt.hash(this.password,10)
-})
+    try {
+        // Hash the password with bcrypt and set it to the password field
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (err) {
+        return next(err);  // pass any error to the next middleware
+    }
+});
+
+// compare the password
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);    
+}
 
 // export the model
-module.exports = mongoose.model('User',userSchema)
+module.exports = mongoose.model('User', userSchema);
