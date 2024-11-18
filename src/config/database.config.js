19b1a4672a8f8677d941848b987d49bd30280a65
Kaium Al Limon <limon.3cs@gmail.com>
2024-11-18 12:20:15 +0600

    config update in both database abd passport

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -1,18 +1,18 @@
 // import mongoose
 const mongoose = require('mongoose');
 
+// configure dotenv
+require('dotenv').config();
+
 // get the database URL from the environment
 const databaseURL = process.env.DB_URL;
+
 
 //connect to the database
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(databaseURL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
+        await mongoose.connect(databaseURL)
 
         console.log("Mongodb connected successfully")
     }catch(error){
---
+++ src/config/passport.config.js
@@ -0,0 +1,60 @@
+// imports
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user.model');
+
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    async (email, password, done) => {
+        try {
+            // find the user
+            const user = await User.findOne({ email })
+
+            // check if the user exists
+
+            if (!user) {
+                return done(null, false, { message: 'User not found' })
+            }
+
+            // check if the password is correct
+
+            const isMatch = await user.matchPassword(password)
+
+            if (!isMatch) {
+                return done(null, false, { message: 'Incorrect password' })
+            }
+
+            // return the user if the password is correct
+            return done(null, user)
+
+
+        } catch (error) {
+            done(error)
+        }
+    }))
+
+
+// serialize the user to store in the session
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+
+// deserialize the user to get the user from the session    
+
+passport.deserializeUser((id, done) => {
+    try {
+        const user = User.findById(id)
+        done(null, user)
+    } catch (error) {
+        done(error)
+    }
+})
+
+// export the passport
+module.exports = passport;
