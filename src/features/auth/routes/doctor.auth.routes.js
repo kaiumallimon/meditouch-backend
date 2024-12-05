cd1e0d97e85773ee49a380f9a49077e09aef718f
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-05 21:32:01 +0600

    permission middleware integration

--- src/features/auth/routes/doctor.auth.routes.js
+++ src/features/auth/routes/doctor.auth.routes.js
@@ -2,19 +2,21 @@
 const express = require('express');
 const authController = require('../controllers/doctor.auth.controller');  // Import the auth controller
 const createUpload = require('../../../utils/image.upload');  // Import createUpload
+const permissionMiddleware = require('../../../middlewares/apikey.middleware');
 const router = express.Router();
+
 
 // Set up multer upload instance with the path where images will be saved
 const upload = createUpload();  // Define the path for image storage
 
 // Define the routes
 // Use the upload.single('profileImage') middleware in the register route to handle the image upload
-router.post('/register', upload.single('image'), authController.register);  
-router.post('/login', authController.login);
-router.get('/doctors', authController.getDoctors);
-router.post('/send-verification-email', authController.sendVerificationCode);  
-router.post('/reset-password/:id', authController.resetPassword);
-router.get('/check-email/', authController.checkEmail);
+router.post('/register',permissionMiddleware('write'), upload.single('image'), authController.register);  
+router.post('/login',permissionMiddleware('read'), authController.login);
+router.get('/doctors',permissionMiddleware('read'), authController.getDoctors);
+router.post('/send-verification-email',permissionMiddleware('read'), authController.sendVerificationCode);  
+router.post('/reset-password/:id',permissionMiddleware('write'), authController.resetPassword);
+router.get('/check-email/',permissionMiddleware('read'), authController.checkEmail);
 
 // Export the router
 module.exports = router;
