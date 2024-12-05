d5f200bec7d38841ab1c2c838f0fda6a52d60d7d
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-05 06:49:45 +0600

    modifications in databse.config

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -7,13 +7,11 @@ require('dotenv').config();
 // get the database URL from the environment
 const databaseURL = process.env.DB_URL;
 
-
 //connect to the database
 
 const connectDB = async()=>{
     try{
         await mongoose.connect(databaseURL)
-
         console.log("Mongodb connected successfully")
     }catch(error){
         console.log("Mongodb connection failed: "+error)
