ab54c59f97f6c4a9491dd6a09f053d1aa4b575ae
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-09 13:12:16 +0600

    cart model modification

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,117 +1,116 @@
-// imports
-const {mongoose} = require('mongoose');
-const { collection } = require('./user.model');
+const mongoose = require('mongoose');
 
 // schema
-
 const cartSchema = new mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User'
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
 
-    medicines:[
+    medicines: [
         {
-            id:{
+            id: {
                 type: String,
                 required: true
             },
 
-            name:{
+            name: {
                 type: String,
                 required: true
             },
 
-            generic_name:{
+            generic_name: {
                 type: String,
                 required: true
             },
 
-            slug:{
+            slug: {
                 type: String,
                 required: true
             },
 
-            category:{
+            category: {
                 type: String,
                 required: true
             },
 
-            manufacturer:{
+            manufacturer: {
                 type: String,
                 required: true
             },
 
-            strength:{
+            strength: {
                 type: String,
                 required: true
             },
 
-            quantity:{
+            quantity: {
                 type: Number,
                 required: true
             },
 
-            selected_unit:{
+            selected_unit: {
                 type: String,
                 required: true
             },
 
-            prices:[
+            prices: [
                 {
-                    price:{
+                    price: {
                         type: Number,
                         required: true
                     },
 
-                    unit:{
+                    unit: {
                         type: String,
                         required: true
                     },
 
-                    unit_size:{
+                    unit_size: {
                         type: Number,
                         required: true
                     }
                 }
             ],
 
-            discount_type:{
+            discount_type: {
                 type: String,
                 required: true
             },
 
-            discount_value:{
+            discount_value: {
                 type: Number,
                 required: true
             },
 
-            image_url:{
+            image_url: {
                 type: String,
                 required: true
             },
 
-            medicine_url:{
+            medicine_url: {
                 type: String,
                 required: true
             },
 
-            added_on:{
+            added_on: {
                 type: Date,
                 default: Date.now
             }
         }
     ]
 
-},{
+}, {
     collection: 'cart',
+    timestamps: true // Automatically adds `createdAt` and `updatedAt` fields
 });
 
-
+// // index user for efficient lookup
+// cartSchema.index({ user: 1 });
 
 // model
 const Cart = mongoose.model('Cart', cartSchema);
-
 
 // export the model
 module.exports = Cart;
