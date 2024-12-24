82888b49dd8af1725c80de47b60ea579db3dc43e
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-25 00:10:32 +0600

    refactor nurse registration route to use multer for handling multiple image uploads

--- src/features/auth/routes/nurse.auth.routes.js
+++ src/features/auth/routes/nurse.auth.routes.js
@@ -1,16 +1,16 @@
 // imports
 const express = require('express');
 const authController = require('../controllers/nurse.auth.controller');  // Import the auth controller
-const createUpload = require('../../../utils/image.upload');  // Import createUpload
 const permissionMiddleware = require('../../../middlewares/apikey.middleware');
+const multer = require('multer');
 const router = express.Router();
 
 
 // Set up multer upload instance with the path where images will be saved
-const upload = createUpload();  // Define the path for image storage
+const upload = multer();  // Define the path for image storage
 
 
-router.post('/register',permissionMiddleware('write'), upload.single('image'), authController.register);  
+router.post('/register',permissionMiddleware('write'), upload.any(), authController.register);  
 router.post('/login',permissionMiddleware('read'), authController.login);
 router.get('/nurses',permissionMiddleware('read'), authController.getNurses);
 router.post('/send-verification-email',permissionMiddleware('read'), authController.sendVerification);  
