b5fb688369b3a553f42d2fb0fdee1903e0a2e08a
Kaium Al Limon <limon.3cs@gmail.com>
2024-12-25 00:10:21 +0600

    refactor image upload handling in community routes to use multer for multiple file uploads

--- src/features/community/routes/community.routes.js
+++ src/features/community/routes/community.routes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const communityController = require('../controllers/community.controller');
-const createUpload = require('../../../utils/image.upload');  // Import createUpload
 const permissionMiddleware = require('../../../middlewares/apikey.middleware');
+const multer = require('multer');
 const router = express.Router();
 
 // Set up multer upload instance with the path where images will be saved
-const upload = createUpload();  // Define the path for image storage
+const upload = multer();  // Define the path for image storage
 
 // Define the routes
 // Use the upload.single('profileImage') middleware in the register route to handle the image upload
 
 
-router.post("/add",permissionMiddleware('write'),upload.single('image'),communityController.addCommunityPosts); // add post
+router.post("/add",permissionMiddleware('write'),upload.any(),communityController.addCommunityPosts); // add post
 
 // get all post and get post by id
 router.get("/get",permissionMiddleware('read'),communityController.getCommunityPosts); // get all post
 router.get("/get/:id",permissionMiddleware('read'),communityController.getCommunityPostById); // get post by id
 
 // update and delete post
-router.put("/update/:id",permissionMiddleware('write'),upload.single('image'),communityController.updateCommunityPost); // update post
+router.put("/update/:id",permissionMiddleware('write'),upload.any(),communityController.updateCommunityPost); // update post
 router.delete("/delete/:id",permissionMiddleware('write'),communityController.deleteCommunityPost); // delete post
 
 //all like and dislike related endpoints
