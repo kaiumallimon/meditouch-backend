fab23e64f3a501edfda9085b372835a3ea76651b
arnob <kamrulislamarnob123@gmail.com>
2024-12-25 22:29:11 +0600

    Rename `/callchat` endpoint to `/sendChat` and add `/getDoc` endpoint

--- src/features/Chatbot/routes/chatbot.routes.js
+++ src/features/Chatbot/routes/chatbot.routes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { runFlowController } = require('../controller/chatbot.controller');
 
-// Rename the endpoint to `/callchat`
-router.post('/callchat', runFlowController);
+
+router.post('/sendChat', runFlowController);
+router.post('/getDoc', runFlowController);
 
 module.exports = router;
